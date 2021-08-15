c32562cce72edb757be1bffcaf414e0b46981ebf
Lucas García Bertaina <70715344+lucasgarciabertaina@users.noreply.github.com>
2021-08-15 11:50:23 -0300

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -18,7 +18,7 @@ echo root:20421846619Lg | chpasswd
 pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font xorg
 
 # pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
