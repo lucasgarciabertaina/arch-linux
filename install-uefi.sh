4192a77c75993cc6ec6bd4ffc5889f57b411b4f8
Lucas García Bertaina <70715344+lucasgarciabertaina@users.noreply.github.com>
2021-08-16 23:27:26 -0300

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -20,7 +20,7 @@ pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_suppl
 # pacman -S --noconfirm xf86-video-amdgpu
 pacman -S nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
